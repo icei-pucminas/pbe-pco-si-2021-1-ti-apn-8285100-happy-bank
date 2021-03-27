aa62018e67ca650384ebf91ac98dca6a9ace8d8b
Hugo José <hugo.jose.ferreira.moreira@gmail.com>
2021-03-27 12:23:23 -0300

    Adicionando melhorias

---
+++ Codigo/Tests/HappyBank.IntegrationTests/HappyData/UserRepositoryTest.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+using Npgsql;
+using HappyBank.Data.Repository;
+using HappyBank.Domain.Model;
+
+namespace HappyBank.IntegrationTests.HappyData
+{
+    public class UserRepositoryTest
+    {
+        private NpgsqlConnection Connection {get; set;}
+
+        public UserRepositoryTest()
+        {
+            var connString = "Host=localhost;Username=postgres;Password=postgres;Database=happybank";
+
+            Connection = new NpgsqlConnection(connString);
+        }
+        
+        [Fact]
+        public void Null_User_Name_Mus_Throw_Exception()
+        {
+            var repository = new UserRepository(Connection);
+            Assert.Throws<InvalidOperationException>(() => repository.Add(new User(null, null)));
+        }
+
+        [Fact]
+        public void Complete_User_Mus_Insert_And_Return_Id()
+        {
+            var repository = new UserRepository(Connection);
+
+            var id = Guid.NewGuid();
+            var newId = repository.Add(new User(id.ToString(), $"User {id}"));
+
+            Assert.NotNull(newId);
+            // Assert.True(id == newId);
+        }
+    }
+}
