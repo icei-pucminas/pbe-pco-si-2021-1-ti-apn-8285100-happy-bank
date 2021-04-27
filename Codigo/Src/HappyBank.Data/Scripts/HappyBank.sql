46c2fa1ddb661069fbc59b7248271160d9b4aba9
Hugo José <hugo.jose.ferreira.moreira@gmail.com>
2021-04-27 08:45:43 -0300

    Adicionando alteracoes

--- Codigo/Src/HappyBank.Data/Scripts/HappyBank.sql
+++ Codigo/Src/HappyBank.Data/Scripts/HappyBank.sql
@@ -11,14 +11,21 @@ CREATE TABLE public.bank
     CONSTRAINT bank_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE public.user 
+CREATE TABLE public.customer
 (
 	id uuid NOT NULL,
-	account_bank uuid,
-    "username" varchar(100) NOT NULL UNIQUE,
 	"password" varchar (64) NOT NULL,
-	CONSTRAINT user_pk PRIMARY KEY (id),
-	CONSTRAINT account_fk FOREIGN KEY (account_bank) REFERENCES public.account(id)
+	govnumber varchar (11) NOT NULL UNIQUE,
+	"name" varchar(100) NOT NULL,
+	street varchar (50) NOT NULL,
+	district varchar (50) NOT NULL,
+	city varchar (50) NOT NULL,
+	"state" varchar (20) NOT NULL,
+	email varchar (100) NOT NULL UNIQUE,
+	phone varchar (20) NOT NULL,
+	address_number varchar (20) NOT NULL,
+	birth_date date NOT NULL DEFAULT NOW(),
+	CONSTRAINT customer_pk PRIMARY KEY (id),
 );
 
 CREATE TABLE public.employees
@@ -37,38 +44,27 @@ CREATE TABLE public.account
 (
     id uuid NOT NULL,
     bank_fk uuid,
+	customer_fk uuid,
 	agency_bank varchar(4) NOT NULL,
 	account_bank varchar (9) NOT NULL UNIQUE,
-	govnumber varchar (11) NOT NULL UNIQUE,
-	access_password varchar (6) NOT NULL,
-	"name" varchar(100) NOT NULL,
-	street varchar (50) NOT NULL,
-	district varchar (50) NOT NULL,
-	city varchar (50) NOT NULL,
-	"state" varchar (20) NOT NULL,
-	email varchar (100) NOT NULL UNIQUE,
-	phone varchar (20) NOT NULL,
-	address_number varchar (20) NOT NULL,
-	birth_date date NOT NULL DEFAULT NOW(),
     CONSTRAINT account_pk PRIMARY KEY (id),
 	CONSTRAINT bank_fk FOREIGN KEY (bank_fk) REFERENCES public.bank(id)
+	CONSTRAINT customer_fk FOREIGN KEY (customer_fk) REFERENCES public.customer(id)
 );
 
 CREATE TABLE public.transaction
 (
     id uuid NOT NULL,
-    bank_fk uuid,
 	acount_bank uuid,
-	user_bank uuid,
 	type_transaction varchar (20) NOT NULL,
 	value_transaction varchar (100) NOT NULL,
-	agency_recipient varchar (4) NOT NULL,
-	account_recipient varchar (9) NOT NULL,
+	terminal_code varchar (50),
+	deposit_code varchar (50),
+	agency_recipient varchar (4),
+	account_recipient varchar (9),
 	date_transaction date NOT NULL DEFAULT NOW(),
     CONSTRAINT transaction_pk PRIMARY KEY (id),
-	CONSTRAINT bank_fk FOREIGN KEY (bank_fk) REFERENCES public.bank(id),
 	CONSTRAINT account_fk FOREIGN KEY (acount_bank) REFERENCES public.account(id),
-	CONSTRAINT user_fk FOREIGN KEY (user_bank) REFERENCES public.user(id)
 );
 
 ALTER TABLE public.bank ADD CONSTRAINT
