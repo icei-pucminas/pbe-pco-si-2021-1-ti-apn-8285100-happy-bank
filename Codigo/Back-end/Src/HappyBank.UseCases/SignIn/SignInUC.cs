9d59cf643eec669c36fc7c0eea45f1b9a92e3c4d
Gustavo Caldeira <caldeira.santos@gmail.com>
2021-05-23 21:04:44 -0300

    SignIN e Cors

---
+++ Codigo/Back-end/Src/HappyBank.UseCases/SignIn/SignInUC.cs
@@ -0,0 +1,37 @@
+using HappyBank.Domain.Repository;
+using HappyBank.Infra.UseCases;
+
+namespace HappyBank.UseCases.SignIn
+{
+    public class SignInUC : IUseCase<SignInInput, SignInOutput>
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly ICustomerRepository _customerRepository;
+        
+        public SignInUC(IAccountRepository accountRepository, ICustomerRepository customerRepository)
+        {
+            _accountRepository = accountRepository;
+            _customerRepository = customerRepository;
+        }
+
+        public SignInOutput Execute(SignInInput input)
+        {
+            var customer = _customerRepository.FindOneByEmail(input.Email);
+
+            if(null != customer && customer.Password.Equals(input.Password))
+            {
+                var accountList = this._accountRepository.FindByCustomerId(customer.Id);
+                if(accountList.Count > 0)
+                {
+                    return new SignInOutput{
+                        CustomerId = customer.Id,
+                        AccountNumber = accountList[0].AccountNumber,
+                        AgencyNumber = accountList[0].AgencyNumber
+                    };
+                }
+            }
+
+            return null;
+        }
+    }
+}
